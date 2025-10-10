b9c6a98254c3d05a75a22c269b74336379a72c38
LukkyClean <guedesdealmeida2708@gmail.com>
2025-10-10 15:23:29 -0300

    fix: warning do pydantic sobre o método Config() descontinuado

--- backend-fastapi/app/core/config.py
+++ backend-fastapi/app/core/config.py
@@ -1,12 +1,14 @@
 # Arquivo para configurações
 
 # Configurações das variáveis com Pydantic Settings Model
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings # type: ignore
+from pydantic import ConfigDict # type: ignore
 
 class Settings(BaseSettings):
     DATABASE_URL: str
 
-    class Config:
-        env_file = ".env"
+    model_config = ConfigDict(
+        env_file=".env"
+    )
 
 settings = Settings()
