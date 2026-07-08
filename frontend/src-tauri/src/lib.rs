3ae59ca7fef1cfb298d5ef124a6c5e50f7d2400e
Carlos André <147477857+dzim-almeida@users.noreply.github.com>
2026-07-08 16:03:55 -0300

    add: servidor escreve arquivo txt com configurações do servidor

--- frontend/src-tauri/src/lib.rs
+++ frontend/src-tauri/src/lib.rs
@@ -1,17 +1,15 @@
 mod backend;
-mod impressao;
 mod config_server;
+mod impressao;
 
 use tauri::{Manager, RunEvent};
 
 use backend::{cleanup_on_exit, AppState};
+use config_server::{get_api_url, get_config, set_role_client, set_role_server};
 use impressao::{
     descobrir_servidores_impressao, imprimir_raw, imprimir_rede, iniciar_servidor_impressao,
     listar_impressoras, obter_ip_local, parar_servidor_impressao, EstadoServidorImpressao,
 };
-use config_server::{load_config, set_role_client, set_role_server, get_api_url, get_config};
-
-use crate::backend::setup_sidecar;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
@@ -33,23 +31,35 @@ pub fn run() {
             get_config
         ])
         .setup(move |app| {
-            let server_config = load_config(app.app_handle());
-
             app.manage(AppState {
                 process: std::sync::Mutex::new(None),
             });
 
-            app.manage(
-                EstadoServidorImpressao::default()
-            );
+            app.manage(EstadoServidorImpressao::default());
 
             #[cfg(not(debug_assertions))]
             {
+                use crate::backend::setup_sidecar;
+                use crate::config_server::gen_network_config_txt;
+                use crate::config_server::load_config;
+
+                let handle = app.app_handle();
+
+                let server_config = load_config(&handle);
                 if server_config.is_server {
-                    setup_sidecar(app.app_handle(), &server_config.server_ip, server_config.server_port)?;
+                    setup_sidecar(
+                        &handle,
+                        &server_config.server_ip,
+                        server_config.server_port,
+                    )?;
+                    gen_network_config_txt(
+                        &handle,
+                        obter_ip_local().unwrap_or_default(),
+                        server_config.server_port,
+                    );
                 }
             }
-        
+
             Ok(())
         })
         .build(tauri::generate_context!())
